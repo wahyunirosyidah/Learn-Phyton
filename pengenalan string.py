8f2240aa04e411921c8939f00955d8885983d7e2
Wahyuni Fajrin Rosyidah <wahyunirosyidah@gmail.com>
2024-06-16 14:04:48 +0900

    selesai pengenalan string

--- pengenalan string.py
+++ pengenalan string.py
@@ -5,5 +5,58 @@ print(type(data))
 #1. Cara membuat string
 
 '''
-
+    1. dengan menggunakan single quote
 '''
+
+data='Menggunakan Single Quote'
+print(data)
+print('"Halo, apa kabar?"')
+data="Menggunakan Double Quote"
+print(data)
+print("'Halo, apa kabar?'")
+
+#agar bisa memasukkan tanda kutip ' di dalam
+#cara 1
+print("ini adalah hari jum'at")
+#cara 2 
+print('ini adalah hari jum\'at')
+print('g\'day, isn\'t it?')
+
+#agar bisa memasukkan backslash (\) di dalam
+print("C:\\user\\Ayu")
+
+#tab
+print("ayu \twahyuni, tab")
+
+#backspace
+print("ayu \bwahyuni, backspace")
+
+#newline
+#LF -> Line Feed, u/ unix, MacOS, linux
+print("baris pertama, \nbaris kedua")
+#CR -> carriage return, u/ commodore,acorn
+print("baris pertama, \rbaris kedua")
+#CRLF -> Line Feed carriage return. u/ windows
+print("baris pertama, \r\nbaris kedua")
+
+#3. String Literal atau raw
+
+#hati-hati
+#print('C:\new\Ayu') -> kalau kayak gini kan harus manual \\
+#cara agar otomatis
+print(r'C:\new\Ayu')
+
+#Multiline String
+print(""""
+      Nama :  Wahyuni
+      Kelas : 3 SD
+      
+      """)
+
+#menggabungkan 
+#multiline literal string dan RAW
+print(r""""
+      Nama :  Wahyuni
+      Kelas : 3 SD
+      website : C:\new\Ayu
+      """)
